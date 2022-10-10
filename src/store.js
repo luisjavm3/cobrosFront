a3931e9f0d5307b141f82b04a1718248f65af494
Luis J. Montes <luisjavm3@gmail.com>
2022-10-10 11:24:12 -0500

    Rename loginSlice to authSlice and move devDependencies

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import authReducer from './Features/Auth/loginSlice';
+import authReducer from './Features/Auth/authSlice';
 
 export const store = configureStore({
   reducer: {
