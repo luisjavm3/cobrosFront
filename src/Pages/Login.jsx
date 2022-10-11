38cc08fb2aa671fde88d94c7fe8b88a2f12000aa
Luis J. Montes <luisjavm3@gmail.com>
2022-10-11 10:53:22 -0500

    Finish Login styling

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
 
 export default function Login() {
   return (
     <div className="login-viewport">
-      <div className="login">Login</div>
+      <div className="login">
+        <div className="login__user-icon">
+          <FontAwesomeIcon icon={faUserSecret} />
+        </div>
+        <div className="login__form-wrapper">
+          <form className="login__form">
+            <div className="login__form-group">
+              <label htmlFor="username">
+                <strong>Usuario</strong>
+                <br />
+                <input type="text" id="username" />
+              </label>
+            </div>
+            <div className="login__form-group">
+              <label htmlFor="password">
+                <strong>Contraseña</strong>
+                <br />
+                <input type="password" id="password" />
+              </label>
+            </div>
+            <div className="login__form-group login__button-wrapper">
+              <button className="button">
+                <strong>Entrar</strong>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
