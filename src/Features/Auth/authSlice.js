fe22bc0245d1c57ce251e0786b5154cc84640e1c
Luis J. Montes <luisjavm3@gmail.com>
2022-10-11 15:35:07 -0500

    Complete Login action

--- src/Features/Auth/authSlice.js
+++ src/Features/Auth/authSlice.js
@@ -1,15 +1,48 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+// import useAxios from '../../Utils/useAxios';
+import axios from 'axios';
 
 const initialState = {
   isAuth: false,
   accessToken: null,
+  loading: false,
 };
+
+export const login = createAsyncThunk(
+  'auth/login',
+  async (loginDto, thunkAPI) => {
+    // const client = useAxios();
+
+    try {
+      const response = await axios.post(
+        'https://localhost:5001/auth/login',
+        loginDto
+      );
+      return response.data;
+    } catch (error) {
+      thunkAPI.rejectWithValue('Cannot login.');
+    }
+  }
+);
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
     foo: (state, action) => {},
+  },
+  extraReducers: {
+    [login.pending]: (state) => {
+      state.loading = true;
+    },
+    [login.fulfilled]: (state, action) => {
+      state.isAuth = true;
+      state.accessToken = action.payload;
+      state.loading = false;
+    },
+    [login.rejected]: (state) => {
+      state.loading = false;
+    },
   },
 });
 
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { useDispatch } from 'react-redux';
+import { login } from '../Features/Auth/authSlice';
 
 export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const submitHandler = function (e) {
+    e.preventDefault();
+
+    dispatch(login({ username, password }));
+  };
+
+  const changeHandler = function (e) {
+    const value = e.target.value;
+    const type = e.target.id;
+
+    switch (type) {
+      case 'username':
+        setUsername(value);
+        break;
+
+      case 'password':
+        setPassword(value);
+        break;
+
+      default:
+        break;
+    }
+  };
+
   return (
     <div className="login-viewport">
       <div className="login">
@@ -10,23 +40,32 @@ export default function Login() {
           <FontAwesomeIcon icon={faUserSecret} />
         </div>
         <div className="login__form-wrapper">
-          <form className="login__form">
+          <form className="login__form" onSubmit={(e) => submitHandler(e)}>
             <div className="login__form-group">
               <label htmlFor="username">
                 <strong>Usuario</strong>
                 <br />
-                <input type="text" id="username" />
+                <input
+                  type="text"
+                  id="username"
+                  onChange={(e) => changeHandler(e)}
+                />
               </label>
             </div>
             <div className="login__form-group">
               <label htmlFor="password">
                 <strong>Contraseña</strong>
                 <br />
-                <input type="password" id="password" />
+                <input
+                  type="password"
+                  id="password"
+                  autoComplete="true"
+                  onChange={(e) => changeHandler(e)}
+                />
               </label>
             </div>
             <div className="login__form-group login__button-wrapper">
-              <button className="button">
+              <button className="button" onClick={submitHandler}>
                 <strong>Entrar</strong>
               </button>
             </div>
