6fea52084f93adffdb94aad51e3c82127efe4175
Luis J. Montes <luisjavm3@gmail.com>
2022-10-11 19:53:14 -0500

    Add Spinner to Login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
-import { useDispatch } from 'react-redux';
-import { login } from '../Features/Auth/authSlice';
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../Features/Auth/authSlice";
+import Spinner from "../Components/Spinner";
 
 export default function Login() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const { loading } = useSelector((store) => store.auth);
   const dispatch = useDispatch();
 
   const submitHandler = function (e) {
@@ -20,11 +22,11 @@ export default function Login() {
     const type = e.target.id;
 
     switch (type) {
-      case 'username':
+      case "username":
         setUsername(value);
         break;
 
-      case 'password':
+      case "password":
         setPassword(value);
         break;
 
@@ -35,43 +37,50 @@ export default function Login() {
 
   return (
     <div className="login-viewport">
-      <div className="login">
-        <div className="login__user-icon">
-          <FontAwesomeIcon icon={faUserSecret} />
+      {!loading ? (
+        <div className="login">
+          <div className="login__user-icon">
+            <FontAwesomeIcon icon={faUserSecret} />
+          </div>
+          <div className="login__form-wrapper">
+            <form className="login__form" onSubmit={(e) => submitHandler(e)}>
+              <div className="login__form-group">
+                <label htmlFor="username">
+                  <strong>Usuario</strong>
+                  <br />
+                  <input
+                    type="text"
+                    id="username"
+                    onChange={(e) => changeHandler(e)}
+                  />
+                </label>
+              </div>
+              <div className="login__form-group">
+                <label htmlFor="password">
+                  <strong>Contraseña</strong>
+                  <br />
+                  <input
+                    type="password"
+                    id="password"
+                    autoComplete="true"
+                    onChange={(e) => changeHandler(e)}
+                  />
+                </label>
+              </div>
+              <div className="login__form-group login__button-wrapper">
+                <button
+                  className="button button--primary"
+                  onClick={submitHandler}
+                >
+                  Entrar
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="login__form-wrapper">
-          <form className="login__form" onSubmit={(e) => submitHandler(e)}>
-            <div className="login__form-group">
-              <label htmlFor="username">
-                <strong>Usuario</strong>
-                <br />
-                <input
-                  type="text"
-                  id="username"
-                  onChange={(e) => changeHandler(e)}
-                />
-              </label>
-            </div>
-            <div className="login__form-group">
-              <label htmlFor="password">
-                <strong>Contraseña</strong>
-                <br />
-                <input
-                  type="password"
-                  id="password"
-                  autoComplete="true"
-                  onChange={(e) => changeHandler(e)}
-                />
-              </label>
-            </div>
-            <div className="login__form-group login__button-wrapper">
-              <button className="button" onClick={submitHandler}>
-                <strong>Entrar</strong>
-              </button>
-            </div>
-          </form>
-        </div>
-      </div>
+      ) : (
+        <Spinner />
+      )}
     </div>
   );
 }
