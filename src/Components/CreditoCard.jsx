b8dc3c26b109b691c3ce045ba34b6465e12c1b0d
Luis J. Montes <luisjavm3@gmail.com>
2022-10-18 17:22:04 -0500

    savepoint

--- src/Components/CreditoCard.jsx
+++ src/Components/CreditoCard.jsx
@@ -1,5 +1,28 @@
 import React from "react";
 
+const abonos = [
+  { id: 1, valor: 12, saldo: 12 },
+  { id: 2, valor: 12, saldo: 12 },
+  { id: 3, valor: 12, saldo: 12 },
+  { id: 4, valor: 12, saldo: 12 },
+  { id: 5, valor: 12, saldo: 12 },
+  { id: 6, valor: 12, saldo: 12 },
+  { id: 7, valor: 12, saldo: 12 },
+  { id: 8, valor: 12, saldo: 12 },
+  { id: 9, valor: 12, saldo: 12 },
+  { id: 10, valor: 12, saldo: 12 },
+  { id: 11, valor: 12, saldo: 12 },
+  { id: 12, valor: 12, saldo: 12 },
+  { id: 13, valor: 12, saldo: 12 },
+  { id: 14, valor: 12, saldo: 12 },
+  { id: 15, valor: 12, saldo: 12 },
+  { id: 16, valor: 12, saldo: 12 },
+  { id: 17, valor: 12, saldo: 12 },
+  { id: 18, valor: 12, saldo: 12 },
+];
+
+const isAdmin = true;
+
 export default function CreditoCard() {
   return (
     <div className="credito-card">
@@ -12,23 +35,72 @@ export default function CreditoCard() {
           <div className="header__fecha">fecha</div>
           <div className="header__numeros">
             <div className="credito">
-              <span>credito</span>
-              <span>100</span>
+              <span className="texto">credito</span>
+              <span className="cifra">100</span>
             </div>
             <div className="interes">
-              <span>interes</span>
-              <span>20%</span>
+              <span className="texto">interes</span>
+              <span className="cifra">20%</span>
             </div>
             <div className="total">
-              <span>total</span>
-              <span>120</span>
+              <span className="texto">total</span>
+              <span className="cifra">120</span>
             </div>
             <div className="saldo">
-              <span>saldo</span>
-              <span>65</span>
+              <span className="texto">saldo</span>
+              <span className="cifra">65</span>
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="abonos">
+        <ul className="lista-abonos">
+          {abonos.map((abono) => {
+            return (
+              <li key={abono.id}>
+                {abono.valor}&nbsp;{abono.saldo}{" "}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      <div className="panel-liquidacion">
+        <div className="input-contenedor">
+          <div className="saldo same-width">
+            <span>saldo</span>
+            <span className="cifra">100</span>
+          </div>
+          <div className="input-wrapper">
+            <form>
+              <input type="text" />
+            </form>
+          </div>
+          <div className="nuevo-saldo same-width">
+            <span>nuevo saldo</span>
+            <span className="cifra">95</span>
+          </div>
+        </div>
+
+        <div className="button-wrapper">
+          <button>abonar</button>
+        </div>
+
+        {isAdmin ? (
+          <div className="dos-opciones">
+            <div className="button-wrapper">
+              <button>nuevo credito</button>
+            </div>
+            <div className="button-wrapper">
+              <button>perdonar</button>
+            </div>
+          </div>
+        ) : (
+          <div className="button-wrapper ">
+            <button>nuevo credito</button>
+          </div>
+        )}
       </div>
     </div>
   );
