41216fdcfd9b398eceed8dbb4dd6f13229f8262f
Luis J. Montes <luisjavm3@gmail.com>
2022-10-19 12:25:08 -0500

    savepoint

--- src/Components/Ruta.jsx
+++ src/Components/Ruta.jsx
@@ -1,143 +1,83 @@
 import React, { useEffect, useState } from "react";
 import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
+import Swal from "sweetalert2";
 
-const reorder = (list, startIndex, endIndex) => {
-  const result = [...list];
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
+// const reorder = (list, startIndex, endIndex) => {
+//   const result = [...list];
+//   const [removed] = result.splice(startIndex, 1);
+//   result.splice(endIndex, 0, removed);
 
-  return result;
-};
+//   return result;
+// };
 
-const initialTasks = [
-  {
-    id: "1",
-    text: "React.js",
-  },
-  {
-    id: "2",
-    text: "HTML/CSS",
-  },
-  {
-    id: "3",
-    text: "AWS",
-  },
-  {
-    id: "4",
-    text: "JavaScript",
-  },
-  {
-    id: "5",
-    text: "Five",
-  },
-  {
-    id: "6",
-    text: "Six",
-  },
-  {
-    id: "7",
-    text: "Seven",
-  },
-  {
-    id: "8",
-    text: "Eight",
-  },
-  {
-    id: "9",
-    text: "Nine",
-  },
-  {
-    id: "10",
-    text: "Ten",
-  },
-  {
-    id: "11",
-    text: "Eleven",
-  },
-  {
-    id: "12",
-    text: "Twelve",
-  },
-  {
-    id: "13",
-    text: "Thirteen",
-  },
-  {
-    id: "14",
-    text: "fourteen",
-  },
-  {
-    id: "15",
-    text: "fifteen",
-  },
-  {
-    id: "16",
-    text: "sixteen",
-  },
-  {
-    id: "17",
-    text: "seventeen",
-  },
-  {
-    id: "18",
-    text: "eighteen",
-  },
-  {
-    id: "19",
-    text: "nineteen",
-  },
-  {
-    id: "20",
-    text: "twenty",
-  },
-  {
-    id: "21",
-    text: "twenty one",
-  },
-  {
-    id: "22",
-    text: "twenty two",
-  },
-];
+const fireRouteModificationAlert = (newPosition) => {};
 
 export default function Ruta(props) {
-  const { client } = props;
-  const [tasks, setTasks] = useState(initialTasks);
+  const { client, id } = props;
+  const [creditos, setCreditos] = useState([]);
 
   useEffect(() => {
-    // async function fetch
-  }, []);
+    console.log("Ruta montada.");
+    async function fetchLoans() {
+      const response = await client.get(`/Cobros/${id}/Loans`);
+      setCreditos(response.data.data);
+    }
+
+    fetchLoans();
+  }, [id, client]);
+
+  const onDragEndHandler = (result) => {
+    const { source, destination } = result;
+
+    if (!destination) return;
+
+    if (source.index === destination.index)
+      if (source.droppableId === destination.droppableId) return;
+
+    const newPosition = destination.index + 1;
+
+    Swal.fire({
+      title: "Estas seguro?",
+      text: `El credito tendrá la posición ${newPosition} en el cobro.`,
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Aceptar",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // const response = await client.pos
+
+        Swal.fire(
+          "Modificado!",
+          `Ahora el credito ocupa la pocision ${newPosition}.`,
+          "success"
+        );
+      }
+    });
+
+    // setTasks((prevTasks) =>
+    //   reorder(prevTasks, source.index, destination.index)
+    // );
+  };
 
   return (
     <div className="ruta">
-      <DragDropContext
-        onDragEnd={(result) => {
-          const { source, destination } = result;
-          if (!destination) {
-            return;
-          }
-          if (
-            source.index === destination.index &&
-            source.droppableId === destination.droppableId
-          ) {
-            return;
-          }
-
-          setTasks((prevTasks) =>
-            reorder(prevTasks, source.index, destination.index)
-          );
-        }}
-      >
+      <DragDropContext onDragEnd={(result) => onDragEndHandler(result)}>
         <div className="app">
-          <Droppable droppableId="tasks">
+          <Droppable droppableId="creditos">
             {(droppableProvided) => (
               <ul
                 className="loan-container"
                 {...droppableProvided.droppableProps}
                 ref={droppableProvided.innerRef}
               >
-                {tasks.map((task, index) => (
-                  <Draggable key={task.id} draggableId={task.id} index={index}>
+                {creditos.map((credito, index) => (
+                  <Draggable
+                    key={credito.id}
+                    draggableId={`${credito.id}`}
+                    index={index}
+                  >
                     {(draggableProvided) => (
                       <li
                         className="loan-item"
@@ -146,9 +86,9 @@ export default function Ruta(props) {
                         {...draggableProvided.dragHandleProps}
                       >
                         <div className="item">
-                          <div className="item__posicion">{index + 1}</div>
-                          <div className="item__cliente">{task.text}</div>
-                          <div className="item__saldo">saldo</div>
+                          <div className="item__posicion">{`${credito.routePosition}`}</div>
+                          <div className="item__cliente">{`${credito.customer.name} ${credito.customer.lastName}`}</div>
+                          <div className="item__saldo">{`$${credito.balance}`}</div>
                         </div>
                       </li>
                     )}
--- src/Pages/Liquidacion.jsx
+++ src/Pages/Liquidacion.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import CreditoCard from "../Components/CreditoCard";
 import Ruta from "../Components/Ruta";
 import { getAxios } from "../Utils/getAxios";
 
 export default function Liquidacion() {
   const client = getAxios();
+  const { id } = useParams();
 
   return (
     <div className="liquidacion-viewport">
       <div className="liquidacion">
         <div className="liquidacion__ruta">
-          <Ruta client={client} />
+          <Ruta client={client} id={id} />
         </div>
         <div className="liquidacion__creditos">
           <div className="credito-actual">
